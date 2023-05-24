de26c513b728364726957520c1723a621f5efd77
Taylor, Joshua-Butah <joshua-butah.taylor@haw-hamburg.de>
2023-05-24 20:12:29 +0200

    sostiges

--- parksystem/src/auto/run.py
+++ parksystem/src/auto/run.py
@@ -14,25 +14,33 @@ from concurrent.futures import ThreadPoolExecutor
 import random
 from Auto import Auto
 
-# -------------------------------------------------------------------------------------------------------------------
-
-
 PARKBUCHTLAENGE = 30
-parkbucht = [False] * PARKBUCHTLAENGE
+parkbucht1 = [False] * PARKBUCHTLAENGE
+parkbucht2 = [False] * PARKBUCHTLAENGE
+parkbucht3 = [False] * PARKBUCHTLAENGE
+
+
+def init():
+    global parkbucht1
+    global parkbucht2
+    global parkbucht3
+
+
 start_index = 15
 end_index = 18
 for i in range(start_index, end_index + 1):
-    parkbucht[i] = True
+    parkbucht1[i] = True
 
-# print(parkbucht)
-
+print(parkbucht1)
 
 random_number = random.randint(0, 30)
 
+def parken(parkbucht, warteschlange, lock):
+
+    return True
+
 
 # logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
-
-# print(random_number)
 def on_message_auto(client, userdata, msg):
     global SENSOR_TOPIC
     ts_iso = msg.payload.decode("utf-8")
@@ -43,52 +51,47 @@ def on_message_auto(client, userdata, msg):
 
 auto2 = Auto(40)
 
-parkbucht = [False] * 2
+# parkbucht1 = [False] * 2
 
 autolist = [auto2]
 
 autotext = ""
 
-def pool(item):
-    global autotext
+
+def autoPool(item):
     autox = Auto(item)
-    autotext += str(autox) + " "
+
     autolist.append(autox)
     zeit = random.randrange(1, 10)
     # logging.info(f'Thread {item}: id= {threading.getident()}')
     logging.info(f'Thread {item}: id= {threading.get_ident()}')
     # logging.info(f'Thread {item}: sleeping for= {zeit} Sekunden')
-    if item % 10 == 9:
-        parkprinter()
-    # time.sleep(zeit)
+    time.sleep(zeit)
     autolist.remove(autox)
     logging.info(f'Thread {item}: finished')
 
+    # global autotext
+    # autotext += str(autox) + " "
+    # if item % 10 == 9:
+    #     parkprinter()
+
 
 def parkprinter():
-
     print(autotext)
 
-
-def task_function(item):
-    print(f"Aufgabe {item} wird ausgeführt.")
-    zeit = random.randrange(1, 10)
-    # logging.info(f'Thread {item}: id= {threading.getident()}')
-    logging.info(f'Thread {item}: id= {threading.get_ident()}')
 
 
 def main():
     logging.basicConfig(format='%(levelname)s - %(asctime)s: %(message)s', datefmt='%H:%M:%S', level=logging.DEBUG)
     logging.info('Threadpool Start')
     worker = 5
-    item = 20
+    item = 1000
 
-    with ThreadPoolExecutor(max_workers=worker) as executor:
-        # while(True):
-        executor.map(pool, range(item))
-
-    logging.info('Threadpool Finished----------')
-    # Erstelle einen ThreadPoolExecutor mit 3 Threads
+    # with ThreadPoolExecutor(max_workers=worker) as executor:
+    #     # while(True):
+    #     executor.map(pool, range(item))
+    #
+    # logging.info('Threadpool Finished----------')
 
 
 if __name__ == '__main__':
