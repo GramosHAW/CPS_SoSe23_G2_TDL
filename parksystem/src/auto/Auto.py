22713833345e886683d93ee7b6eed6eceafc7cf8
Taylor, Joshua-Butah <joshua-butah.taylor@haw-hamburg.de>
2023-05-25 11:44:27 +0200

    Mqtt auskommentiert

--- parksystem/src/auto/Auto.py
+++ parksystem/src/auto/Auto.py
@@ -5,7 +5,7 @@ import random as rd
 import sys
 import json
 from random import seed
-from mqtt.mqtt_wrapper import MQTTWrapper
+#from mqtt.mqtt_wrapper import MQTTWrapper
 
 class Auto:
     # Variablen die wir noch bauchen
@@ -14,7 +14,7 @@ class Auto:
     # parksucheDauer = Dauer der Parkplatz suche
     # lebenszeit = länge der Lebenszeit in der Simulation, seit dem erzeugen
 
-    liste_aller_Auto_ID = set()
+    #liste_aller_Auto_ID = set()
 
     def __init__(self, id):
 
@@ -52,24 +52,23 @@ class Auto:
 
     DATA_TOPIC = "Auto/Einparken"
 
-    def __on_message_tick(self, client, userdata, msg):
-        global DATA_TOPIC
-        #ts_iso = msg.payload.decode("utf-8")
-        #value = randint(0, 100)
-        data = {"payload": "", "timestamp": 1}
-        client.publish(DATA_TOPIC, json.dumps(data))
-
-    def topics_subscriben(self):
-        SEED = 10
-        seed(SEED)
-        mqtt = MQTTWrapper('mqttbroker', 1883, name='auto_1')
-        for topic in self.__liste_aller_topic_in:
-            mqtt.subscribe(topic)
-            mqtt.subscribe_with_callback(topic, self.__on_message_tick)
-
-        try:
-            mqtt.loop_forever()
-        except(KeyboardInterrupt, SystemExit):
-            mqtt.stop()
-            sys.exit("KeyboardInterrupt -- shutdown gracefully.")
-
+    # def __on_message_tick(self, client, userdata, msg):
+    #     global DATA_TOPIC
+    #     #ts_iso = msg.payload.decode("utf-8")
+    #     #value = randint(0, 100)
+    #     data = {"payload": "", "timestamp": 1}
+    #     client.publish(DATA_TOPIC, json.dumps(data))
+    #
+    # def topics_subscriben(self):
+    #     SEED = 10
+    #     seed(SEED)
+    #     mqtt = MQTTWrapper('mqttbroker', 1883, name='auto_1')
+    #     for topic in self.__liste_aller_topic_in:
+    #         mqtt.subscribe(topic)
+    #         mqtt.subscribe_with_callback(topic, self.__on_message_tick)
+    #
+    #     try:
+    #         mqtt.loop_forever()
+    #     except(KeyboardInterrupt, SystemExit):
+    #         mqtt.stop()
+    #         sys.exit("KeyboardInterrupt -- shutdown gracefully.")
